159bfd116f99d6928922d25f06c66376043ef4cc
ayberk.yavuz <ayberk.yavuz@n11.com>
2022-06-16 18:05:08 +0300

    notes added to PrimitiveDataTypes.

--- main_module/src/subjects/PrimitiveDataTypes.java
+++ main_module/src/subjects/PrimitiveDataTypes.java
@@ -2,6 +2,8 @@ package subjects;
 
 public class PrimitiveDataTypes {
     // one byte is 8 bits. A bit can be 0 or 1.
+    // it can store a number (-128 to 127).
+    // it can store characters. If you need characters, use char data type instead of byte.
     private byte myByte;
 
     public PrimitiveDataTypes() {
