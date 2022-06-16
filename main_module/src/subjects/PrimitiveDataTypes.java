35eeda66d9cf93e6db062196fcc07c638b7e4e5b
ayberk.yavuz <ayberk.yavuz@n11.com>
2022-06-16 17:52:53 +0300

    PrimitiveDataTypes was created.

---
+++ main_module/src/subjects/PrimitiveDataTypes.java
@@ -0,0 +1,17 @@
+package subjects;
+
+public class PrimitiveDataTypes {
+    // one byte is 8 bits. A bit can be 0 or 1.
+    private byte myByte;
+
+    public PrimitiveDataTypes() {
+    }
+
+    public byte getMyByte() {
+        return myByte;
+    }
+
+    public void setMyByte(byte myByte) {
+        this.myByte = myByte;
+    }
+}
